2a7b57c53a18028bbfaf6546d84719a7fdfc695c
Jim Dunne <Jim_Dunne@ultimatesoftware.com>
2018-04-19 14:24:45 -0500

    Move old UART MIDI code to separate file. New midi.c file for generic open/write MIDI.

--- raspberrypi/midi-uart0.c
+++ raspberrypi/midi-uart0.c
@@ -6,28 +6,62 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+#define termios asmtermios
+#define termio asmtermio
+#define winsize asmwinsize
+#include <asm/termios.h>
+#undef  winsize
+#undef  termio
+#undef  termios
 #include <sys/ioctl.h>
 
 #include "types.h"
 #include "hardware.h"
 
-// Global variable for holding file descriptor to talk to MIDI communications:
-int midi_fd = -1;
+// Global variable for holding file descriptor to talk to UART0 for MIDI communications:
+int uart0_fd = -1;
 
-// Use "Fore" USB-MIDI adapter device on Raspberry Pi 3:
-// P:  Vendor=552d ProdID=4348 Rev=02.11
-// S:  Product=USB Midi
-const char *midi_fname = "/dev/snd/by-id/usb-552d_USB_Midi-00";
+// Default UART0 device name on Raspberry Pi Model B:
+const char *midi_fname = "/dev/ttyAMA0";
 
 // Open UART0 device for MIDI communications and set baud rate to 31250 per MIDI standard:
 int midi_init(void) {
-    midi_fd = open(midi_fname, O_WRONLY | O_NONBLOCK);
-    if (midi_fd == -1) {
+#ifdef __linux
+    struct termios2 tio;
+#endif
+
+    uart0_fd = open(midi_fname, O_RDWR | O_NOCTTY | O_NONBLOCK);
+    if (uart0_fd == -1) {
         char err[100];
         sprintf(err, "open('%s')", midi_fname);
         perror(err);
         return 1;
     }
+
+    // Requirements:
+    //   * UART0 is enabled
+    //   * UART0 is unused by the system for any ttys (remove mention of ttyAMA0 from `/etc/inittab`)
+    //   * UART0 clock rate is set to default of 3MHz in /boot/config.txt
+    //     * `init_uart_clock=3000000`
+    //   * UART0 clock rate for Linux is set to default of 3MHz in /boot/cmdline.txt
+    //     * `bcm2708.uart_clock=3000000`
+    //
+    // NOTE: Any hacks to change the default UART clock speed of the RPi B to adjust the
+    // standard baud rates of 38400 to actually be ~31250 DO NOT WORK on latest Raspbian
+    // kernels. I'm using `Linux 4.1.18 armv6l` as of 2017-02-20.
+    //
+    // The only thing that DOES work on latest Raspbian kernels is the code below which uses
+    // termios2 to set a custom baud rate of 31250 which is the MIDI standard.
+    
+    // Set baud rate to 31250 for MIDI:
+#ifdef __linux
+    ioctl(uart0_fd, TCGETS2, &tio);
+    tio.c_cflag &= ~CBAUD;
+    tio.c_cflag |= BOTHER;
+    tio.c_ispeed = 31250;
+    tio.c_ospeed = 31250;
+    ioctl(uart0_fd, TCSETS2, &tio);
+#endif
 
     return 0;
 }
@@ -37,7 +71,7 @@ void midi_send_cmd1_impl(u8 cmd_byte, u8 data1) {
     u8 buf[2];
     buf[0] = cmd_byte;
     buf[1] = data1;
-    count = write(midi_fd, buf, 2);
+    count = write(uart0_fd, buf, 2);
     if (count != 2) {
         perror("Error sending MIDI bytes");
         return;
@@ -51,7 +85,7 @@ void midi_send_cmd2_impl(u8 cmd_byte, u8 data1, u8 data2) {
     buf[0] = cmd_byte;
     buf[1] = data1;
     buf[2] = data2;
-    count = write(midi_fd, buf, 3);
+    count = write(uart0_fd, buf, 3);
     if (count != 3) {
         perror("Error sending MIDI bytes");
         return;
@@ -86,7 +120,7 @@ void midi_send_sysex(u8 byte) {
         }
         printf("\n");
 
-        ssize_t count = write(midi_fd, sysex, write_count);
+        ssize_t count = write(uart0_fd, sysex, write_count);
         if (count < 0) {
             perror("write in midi_send_sysex");
             return;
