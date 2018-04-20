135f1f1df4c22a541d1933bcbda788878b839c4c
Jim Dunne <Jim_Dunne@ultimatesoftware.com>
2018-04-19 20:57:33 -0500

    Remove old build scripts in favor CMake.

--- raspberrypi/cross-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# http://www.welzels.de/blog/en/arm-cross-compiling-with-mac-os-x/
-CC=/usr/local/linaro/arm-linux-gnueabihf-raspbian/bin/arm-linux-gnueabihf-gcc
-
-SRCS=(main.c ../common/controller-axe.c flash.c i2c.c fsw.c lcd.c leds.c midi.c)
-
-$CC -g -o rpi1b -DHW_VERSION=4 -I../common "${SRCS[@]}"
